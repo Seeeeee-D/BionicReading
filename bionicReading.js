e932d1ce573050b61c24523e148689d76712481e
mei <mei28aquarius@gmail.com>
2022-05-21 11:59:35 +0900

    単語の先頭は太文字になるよ
    Co-authored-by: nanaka0012 github.com>
    Co-authored-by: KindMaple ithub.com>

--- bionicReading.js
+++ bionicReading.js
@@ -1,3 +1,25 @@
+/**
+ * BionicReadingを適用した文字列を返す
+ */
+function boldify(text) {
+    // 単語に分解
+    const words = text.split(" ")
+    // それぞれ前半だけ太字に
+    const newWords = [];
+    words.map((w)=>{
+      const wSize = Math.floor(w.length / 2);
+      const first = w.substring(0, wSize).bold();
+      const last = w.substring(wSize, w.length);
+      console.log(`first: ${first}`)
+      console.log(`last: ${last}`)
+      newWords.push(first.concat(last));
+    });
+    console.log(`text: ${text}`)
+    console.log(`newText: ${newWords.join(" ")}`)
+    // 連結して返す
+    return newWords.join(" ");
+}
+
 document.body.onclick = (e) => {
     // 始点
     const x = e.pageX;
@@ -16,10 +38,10 @@ document.body.onclick = (e) => {
     if(!selection.rangeCount) return; //範囲選択されている箇所がない場合は何もせず終了
 
     let selectedRange = selection.getRangeAt(0);
-    let newNode = document.createElement('strong');
+    let newNode = document.createElement('span');
     // ボールドにする
     // range.toString()が使えるからやっぱりselectionいらないかも
-    newNode.innerHTML = selection.toString();
+    newNode.innerHTML = boldify(selection.toString());
     // 元のテキストを削除して
     selectedRange.deleteContents();
     // 装飾済みテキストを挿入
